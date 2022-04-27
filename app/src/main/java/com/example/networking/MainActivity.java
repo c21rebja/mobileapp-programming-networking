c9057a0d684236a31c1917aceaecef60622fd896
c21rebja <c21rebja@student.his.se>
2022-04-27 15:07:36 +0200

    Added gson parsing from DEMO-project

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,8 +5,19 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Scanner;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String TAG = "===";
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
@@ -22,6 +33,18 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> listOfMountains = gson.fromJson(json, type);
+        if(listOfMountains != null) {
+            Log.d(TAG, "Number of elements: " + listOfMountains.size());
+            Log.d(TAG, "Element 0: " + listOfMountains.get(0).toString());
+        }
+        else {
+            Log.d(TAG, "There were no elements to show.");
+        }
+
     }
 
 }
