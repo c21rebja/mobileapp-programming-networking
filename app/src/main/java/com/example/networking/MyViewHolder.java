de20556fa4872749fd1f946cbc3263472b343d75
c21rebja <c21rebja@student.his.se>
2022-04-27 16:38:52 +0200

    fixed errors from MyViewHolder

--- app/src/main/java/com/example/networking/MyViewHolder.java
+++ app/src/main/java/com/example/networking/MyViewHolder.java
@@ -4,12 +4,13 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MyViewHolder {
+public class MyViewHolder extends RecyclerView.ViewHolder {
     TextView name;
 
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
-        name = itemView.findViewById(R.id.);
+        name = itemView.findViewById(R.id.item_name);
     }
 }
